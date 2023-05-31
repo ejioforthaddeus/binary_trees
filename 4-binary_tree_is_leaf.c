37160d14151c304f59cc9f9209bf75d890ae8bd0
ejioforthadeus <ejioforthaddeus037@gmail.com>
2023-05-31 20:11:59 +0100

    tree leave commit

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -2,10 +2,10 @@
 
 /**
  * binary_tree_is_leaf - Checks if a node is a leaf of a binary tree.
- * @node: A pointer.
+ * @node: A pointer to the node to check.
  *
  * Return: If the node is a leaf - 1.
- *         Else - 0.
+ *         Otherwise - 0.
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
